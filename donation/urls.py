4f366fa69b0c900c4b165728901ec234b9a8e07c
evansam119 <b00705016@ntu.edu.tw>
2014-10-21 23:23:02 +0800

    sdf

--- donation/urls.py
+++ donation/urls.py
@@ -3,5 +3,5 @@ from donation.views import *
 
 urlpatterns = patterns('',
 	url(r'^query/$', QueryView.as_view()),
-	url(r'^donateInvoice/$', DonateInvoiceView.as_view()),
+	url(r'^invoice1/$', DonateInvoiceView.as_view()),
  )
