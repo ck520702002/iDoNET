bad1a871128ce00b87922357d8914699972a1472
jeanhuang78 <jean@jeanMacBookAir.local>
2014-10-21 23:07:08 +0800

    good

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,11 +6,6 @@ class login_form(forms.Form):
     password = forms.CharField(label='密碼',widget=forms.PasswordInput)
 
 class sign_up_form(forms.Form):
-<<<<<<< HEAD
     username = forms.IntegerField(label='卡號')
     password = forms.CharField(label='密碼',widget=forms.PasswordInput)
     password_again = forms.CharField(label='確認密碼',widget=forms.PasswordInput)
-=======
-    username = forms.CharField(label='卡號')
-    password = forms.CharField(label='密碼',widget=forms.PasswordInput)
->>>>>>> 04780fbcc9d201b639de3dafba81847b9f7f88fc
