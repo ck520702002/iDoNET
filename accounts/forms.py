c310f53aa5916fb000deefe523d3204379d7e21f
jeanhuang78 <jean@jeanMacBookAir.local>
2014-10-21 22:58:53 +0800

    whatQQ Please enter the commit message for your changes. Lines starting

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,3 +8,4 @@ class login_form(forms.Form):
 class sign_up_form(forms.Form):
     username = forms.IntegerField(label='卡號')
     password = forms.CharField(label='密碼',widget=forms.PasswordInput)
+    password_again = forms.CharField(label='確認密碼',widget=forms.PasswordInput)
