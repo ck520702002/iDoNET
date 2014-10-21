20381f193aceb5fccd0c03f5c69fe5af616195c8
ck520702002 <ck520702002@gmail.com>
2014-10-21 23:46:29 +0800

    fix conflicts

--- apis/views.py
+++ apis/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def get_product_price():
+	product_id = request.POST['product_id']
+	product_id = request.POST['product_id']
 
 def get_threshold_and_roundup():
 
