36db65ecd4bf610539e025505b8456e268f26174
ck520702002 <ck520702002@gmail.com>
2014-10-24 20:19:09 +0800

    fixed conflict

--- donation/models.py
+++ donation/models.py
@@ -33,12 +33,11 @@ class Donation_detail(models.Model):
 	owner = models.ForeignKey(User)
 	threshold = models.IntegerField(max_length=20,null=True)
 	roundup = models.IntegerField(max_length=20,null=True)
-	shopping_amount = models.IntegerField(max_length=10,blank=False)
-	donation_amount = models.IntegerField(max_length=10,blank=False)
+	amount = models.IntegerField(max_length=10,blank=False)
 	invoice = models.ForeignKey(Invoice)
 	time = models.DateTimeField(auto_now=True)
 	to_whom = models.ForeignKey(Charity)
 	Donation_detailamount = models.IntegerField(max_length=10,blank=True,null=True)
 	def __unicode__(self):  
-          return self.time
+          return self.id
 
